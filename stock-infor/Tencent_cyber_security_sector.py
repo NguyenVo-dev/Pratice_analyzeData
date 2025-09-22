d231c6aa430dc0d833b627fc07ce6ee6d82e9a50
NguyenVo-dev <ite11012006@gmail.com>
2025-09-21 13:45:54 -1200

    Update Tencent_cyber_security_sector.py

--- stock-infor/Tencent_cyber_security_sector.py
+++ stock-infor/Tencent_cyber_security_sector.py
@@ -19,3 +19,7 @@ plt.ylabel('Normalized Price')
 plt.legend(normalized_data.columns) # Use the column names (tickers) as labels
 plt.grid(True)
 plt.show()
+#x=High_TME, y=High_TCTFZ, z=High_HACK, o=High_IHAK
+#Close_at_month_3=recent/close==>close_at_month_3 increase==>du dinh alert if ratio more than.....
+#Find the ratio that we should give up the stock
+
